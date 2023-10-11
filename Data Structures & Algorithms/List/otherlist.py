736f2f03a58098b581ae61e30dcfd5e0d0f2a374
zakaria saif <129240583+zacksfF@users.noreply.github.com>
2023-10-11 12:55:23 +0100

    Add files via upload

---
+++ Data Structures & Algorithms/List/otherlist.py
@@ -0,0 +1,12 @@
+thislist = ["apple", "banana", "cherry"]
+mylist = thislist.copy()
+print(mylist)
+
+mylist = list(thislist)
+print(mylist)
+
+# Joins 
+list1 = ["tokyo", "NyC", "Paris", "caasa"]
+list2 = ["1", "2", "3"]
+list3 = list1 + list2
+print(list3)
