ee0ad71c6421b73db8eac8b50be9ae5c1430bc85
zakaria saif <129240583+zacksfF@users.noreply.github.com>
2023-10-13 17:36:47 +0100

    Add files via upload

---
+++ Variables, Expressions and statement/variables.py
@@ -0,0 +1,23 @@
+my_firstname = "zakaria"
+last_name = "saif"
+passion = "Be_better_AI_and_system_engineer"
+Age = 21
+Programming = ("Python", "Rust")
+skills = "machine learning, AI, backend, DevOps, Data Engineer familiar with Quantum computing"
+is_married = "no i am still in 21 hh"
+person_info = {
+    'firstname':'arae', 
+    'lastname':'erer', 
+    'country':'free',
+    'city':'refef'
+    }
+
+#print
+print("my_firstname:", my_firstname)
+print("last_name:",last_name)
+print("passion:", passion)
+print("Age:", Age)
+print("Programming :", Programming)
+print("skills :", skills)
+print("is_married :",is_married )
+print("person_info", person_info)
